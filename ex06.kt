fca80b0b3aaa43912649fd7049500d34b97056e9
Debora <50639601+daspiccolo@users.noreply.github.com>
2019-05-17 15:57:03 -0300

    Update ex06.kt

--- ex06.kt
+++ ex06.kt
@@ -1,23 +1,24 @@
 package exercicios
 
-import org.omg.CORBA.INTERNAL
-import java.lang.Math.pow
-
+import kotlin.math.pow
 
 fun main(args:Array<String>){
 
     println("${VERMELHO}----------------------------------------")
     println("           CALCULO POTENCIA")
     println("----------------------------------------")
+    
     print("${BRANCO}Base:")
     val n1: Double= readLine()!!.toDouble()
+    
     print("${BRANCO}Expoente:")
     val n2: Double= readLine()!!.toDouble()
-    var pot:Double = pow(n1,n2)
+      
+    var pot:Float = n1.toFloat().pow(n2)
 
     println("${AMARELO}---------------RESULTADOS---------------")
-    println("O numeros digitados foram $n1 e $n2")
-    println("A potencia é $pot")
-    println("----------------------------------------")
+    println("O numeros digitados foram $AZUL$n1 e $AZUL$n2")
+    println("A potencia é $AZUL$pot")
+    println("${AMARELO}----------------------------------------")
 
 }
